b3cbd8d374e320e08c7d7bb9171bcf3138e03100
axiaoxin <254606826@qq.com>
2020-08-21 14:42:43 +0800

    Revert "viper config callback set a type"
    This reverts commit 1b88dcadc6811bbb625ffa34799fbb5d6c5505ce.

--- viper.go
+++ viper.go
@@ -7,15 +7,12 @@ import (
 	"github.com/spf13/viper"
 )
 
-// OnVipConfigChangeRun vip config文件变化时的回调函数类型
-type OnVipConfigChangeRun func(fsnotify.Event)
-
 // InitViper 根据配置文件路径和名称初始化 viper 并监听变化
 // configPath 配置文件路径
 // configName 配置文件名（不带格式后缀）
 // configType 配置文件格式后缀
 // onConfigChangeRun 配置文件发生变化时的回调函数
-func InitViper(configPath, configName, configType string, run OnVipConfigChangeRun) error {
+func InitViper(configPath, configName, configType string, onConfigChangeRun func(fsnotify.Event)) error {
 	viper.AddConfigPath(configPath)
 	viper.SetConfigName(configName)
 	viper.SetConfigType(configType)
@@ -23,8 +20,8 @@ func InitViper(configPath, configName, configType string, run OnVipConfigChangeR
 		return err
 	}
 	viper.WatchConfig()
-	if run != nil {
-		viper.OnConfigChange(run)
+	if onConfigChangeRun != nil {
+		viper.OnConfigChange(onConfigChangeRun)
 	}
 	return nil
 }
