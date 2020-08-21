7170ef4a00b1ba017dd3ed6634373c38cfbb3135
axiaoxin <254606826@qq.com>
2020-08-21 14:36:47 +0800

    viper config callback set a type

--- viper.go
+++ viper.go
@@ -7,12 +7,15 @@ import (
 	"github.com/spf13/viper"
 )
 
+// OnVipConfigChangeRun vip config文件变化时的回调函数类型
+type OnVipConfigChangeRun func(fsnotify.Event)
+
 // InitViper 根据配置文件路径和名称初始化 viper 并监听变化
 // configPath 配置文件路径
 // configName 配置文件名（不带格式后缀）
 // configType 配置文件格式后缀
 // onConfigChangeRun 配置文件发生变化时的回调函数
-func InitViper(configPath, configName, configType string, onConfigChangeRun func(fsnotify.Event)) error {
+func InitViper(configPath, configName, configType string, run OnVipConfigChangeRun) error {
 	viper.AddConfigPath(configPath)
 	viper.SetConfigName(configName)
 	viper.SetConfigType(configType)
@@ -20,8 +23,8 @@ func InitViper(configPath, configName, configType string, onConfigChangeRun func
 		return err
 	}
 	viper.WatchConfig()
-	if onConfigChangeRun != nil {
-		viper.OnConfigChange(onConfigChangeRun)
+	if run != nil {
+		viper.OnConfigChange(run)
 	}
 	return nil
 }
