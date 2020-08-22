4b59180d2182dce23a75917894850afd826a286e
axiaoxin <254606826@qq.com>
2020-08-22 11:35:38 +0800

    add httptest

---
+++ httptest.go
@@ -0,0 +1,49 @@
+package goutils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+)
+
+// RequestHTTPHandler 根据参数请求传入的 http.Handler 对应的 path 接口，用于接口测试
+// 返回 ResponseRecorder: https://golang.org/pkg/net/http/httptest/#ResponseRecorder
+func RequestHTTPHandler(app http.Handler, method, path string, body []byte, contentType ...string) (*httptest.ResponseRecorder, error) {
+	req, err := http.NewRequest(strings.ToUpper(method), path, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	if len(contentType) > 0 {
+		req.Header.Set("Content-Type", contentType[0])
+	}
+	rsp := httptest.NewRecorder()
+	app.ServeHTTP(rsp, req)
+	return rsp, nil
+}
+
+// RequestHTTPHandlerFunc 根据参数请求传入的 http.HandlerFunc 返回请求处理结果 body ，用于接口测试
+func RequestHTTPHandlerFunc(f http.HandlerFunc, method string, body []byte, contentType ...string) ([]byte, error) {
+	server := httptest.NewServer(http.HandlerFunc(f))
+	defer server.Close()
+
+	client := &http.Client{}
+	req, err := http.NewRequest(strings.ToUpper(method), server.URL, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	if len(contentType) > 0 {
+		req.Header.Set("Content-Type", contentType[0])
+	}
+	rsp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer rsp.Body.Close()
+	rspBody, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return rspBody, nil
+}
---
+++ httptest_test.go
@@ -0,0 +1,72 @@
+package goutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestRequestHTTPHandler(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	app := gin.New()
+	app.GET("/api/get", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data": "get",
+		})
+	})
+
+	getrecorder, err := RequestHTTPHandler(app, "GET", "/api/get", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if getrecorder.Code != 200 {
+		t.Error("code = ", getrecorder.Code)
+	}
+
+	jsonresp := struct {
+		Data string `json:"data"`
+	}{}
+
+	json.Unmarshal(getrecorder.Body.Bytes(), &jsonresp)
+	if jsonresp.Data != "get" {
+		t.Error("data field value error:" + jsonresp.Data)
+	}
+
+	postdata := struct {
+		Name string `json:"name"`
+	}{}
+	app.POST("/api/post", func(c *gin.Context) {
+		c.ShouldBindJSON(&postdata)
+		c.JSON(200, gin.H{
+			"data": postdata.Name,
+		})
+	})
+	body := []byte(`{"name": "axiaoxin"}`)
+	postrecorder, err := RequestHTTPHandler(app, "POST", "/api/post", body, "application/json")
+	if err != nil {
+		t.Error(err)
+	}
+	if postrecorder.Code != 200 {
+		t.Error("code = ", getrecorder.Code)
+	}
+	json.Unmarshal(postrecorder.Body.Bytes(), &jsonresp)
+	if jsonresp.Data != "axiaoxin" {
+		t.Error("data field value error:" + jsonresp.Data)
+	}
+}
+
+func TestRequestHTTPHandlerFunc(t *testing.T) {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello!")
+	}
+	b, err := RequestHTTPHandlerFunc(f, "GET", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b) != "Hello!" {
+		t.Error(string(b))
+	}
+}
