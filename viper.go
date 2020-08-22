8a4cc9c52d70f95abb7210619140069fb3d59425
axiaoxin <254606826@qq.com>
2020-08-22 19:57:29 +0800

    调整函数位置

--- viper.go
+++ viper.go
@@ -3,12 +3,7 @@
 package goutils
 
 import (
-	"flag"
-	"os"
-
-	"github.com/axiaoxin-com/logging"
 	"github.com/fsnotify/fsnotify"
-	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
@@ -35,41 +30,4 @@ func InitViper(configPath, configName, configType string, onConfigChangeRun func
 // IsInitedViper 返回 viper 是否已初始化
 func IsInitedViper() bool {
 	return viper.GetBool("viper.inited")
-}
-
-// InitWebAppViperConfig 按 viper webapp 配置文件初始化 viper 配置
-func InitWebAppViperConfig() {
-	// 加载配置文件到 viper
-	workdir, err := os.Getwd()
-	if err != nil {
-		logging.Warn(nil, "get workdir failed:"+err.Error())
-		workdir = "."
-	}
-	configPath := flag.String("p", workdir, "path of config file")
-	configName := flag.String("c", "viper.webapp", "name of config file without format suffix)")
-	configType := flag.String("t", "toml", "type of config file format")
-	flag.Parse()
-	if err := InitViper(*configPath, *configName, *configType, func(e fsnotify.Event) {
-		logging.Warn(nil, "Config file changed:"+e.Name)
-	}); err != nil {
-		logging.Warn(nil, "Init viper error:"+err.Error())
-	}
-
-	// 设置配置默认值
-	viper.SetDefault("env", "dev")
-
-	viper.SetDefault("server.addr", ":4869")
-	viper.SetDefault("server.mode", gin.ReleaseMode)
-	viper.SetDefault("server.pprof", true)
-	viper.SetDefault("server.read_timeout", 5)  // 服务器从 accept 到读取 body 的超时时间（秒）
-	viper.SetDefault("server.write_timeout", 5) // 服务器从 accept 到写 response 的超时时间（秒）
-
-	viper.SetDefault("apidocs.title", "pink-lady swagger apidocs")
-	viper.SetDefault("apidocs.desc", "Using pink-lady to develop gin app on fly.")
-	viper.SetDefault("apidocs.host", "localhost:4869")
-	viper.SetDefault("apidocs.basepath", "/")
-	viper.SetDefault("apidocs.schemes", []string{"http"})
-
-	viper.SetDefault("basic_auth.username", "admin")
-	viper.SetDefault("basic_auth.password", "admin")
 }
--- webapp.go
+++ webapp.go
@@ -2,6 +2,7 @@ package goutils
 
 import (
 	"context"
+	"flag"
 	"net/http"
 	"os"
 	"os/signal"
@@ -9,10 +10,50 @@ import (
 	"time"
 
 	"github.com/axiaoxin-com/logging"
+	"github.com/fsnotify/fsnotify"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
+// InitWebAppViperConfig 按 viper webapp 配置文件初始化 viper 配置
+func InitWebAppViperConfig() {
+	// 加载配置文件到 viper
+	workdir, err := os.Getwd()
+	if err != nil {
+		logging.Warn(nil, "get workdir failed:"+err.Error())
+		workdir = "."
+	}
+	configPath := flag.String("p", workdir, "path of config file")
+	configName := flag.String("c", "viper.webapp", "name of config file without format suffix)")
+	configType := flag.String("t", "toml", "type of config file format")
+	flag.Parse()
+	if err := InitViper(*configPath, *configName, *configType, func(e fsnotify.Event) {
+		logging.Warn(nil, "Config file changed:"+e.Name)
+	}); err != nil {
+		logging.Warn(nil, "Init viper error:"+err.Error())
+	}
+
+	// 设置配置默认值
+	viper.SetDefault("env", "dev")
+
+	viper.SetDefault("server.addr", ":4869")
+	viper.SetDefault("server.mode", gin.ReleaseMode)
+	viper.SetDefault("server.pprof", true)
+	viper.SetDefault("server.read_timeout", 5)  // 服务器从 accept 到读取 body 的超时时间（秒）
+	viper.SetDefault("server.write_timeout", 5) // 服务器从 accept 到写 response 的超时时间（秒）
+
+	viper.SetDefault("apidocs.title", "pink-lady swagger apidocs")
+	viper.SetDefault("apidocs.desc", "Using pink-lady to develop gin app on fly.")
+	viper.SetDefault("apidocs.host", "localhost:4869")
+	viper.SetDefault("apidocs.basepath", "/")
+	viper.SetDefault("apidocs.schemes", []string{"http"})
+
+	viper.SetDefault("basic_auth.username", "admin")
+	viper.SetDefault("basic_auth.password", "admin")
+}
+
 // RunWebApp 以 viper 加载的 webapp 配置启动运行 http.Handler 的 app
+// 注意：这里依赖 viper ，必须在外部先对 viper 配置进行加载
 func RunWebApp(app http.Handler, routesRegister func(http.Handler)) {
 	// 结束时关闭 db 连接
 	defer CloseGormInstances()
