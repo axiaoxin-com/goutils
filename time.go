c24cc2bd6b48055e6066f263e8e8658515fc7be9
axiaoxin <254606826@qq.com>
2020-10-11 16:43:50 +0800

    add StrToTime

--- time.go
+++ time.go
@@ -2,7 +2,18 @@
 
 package goutils
 
+import "time"
+
 // UnixTimestampTrim 将 Unix 时间戳（秒）以指定秒数进行规整
 func UnixTimestampTrim(ts int64, sec int64) int64 {
 	return ts - ts%sec
+}
+
+// StrToTime 字符串时间转 time 对象
+func StrToTime(layout, value string, loc ...*time.Location) (time.Time, error) {
+	location := time.Local
+	if len(loc) > 0 {
+		location = loc[0]
+	}
+	return time.ParseInLocation(layout, value, location)
 }
