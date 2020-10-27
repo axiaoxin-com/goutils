b65a36f2205ab51d299b835d267b93cdce3b836f
axiaoxin <254606826@qq.com>
2020-10-27 14:54:28 +0800

    strtotime default use utc

--- time.go
+++ time.go
@@ -11,7 +11,7 @@ func UnixTimestampTrim(ts int64, sec int64) int64 {
 
 // StrToTime 字符串时间转 time 对象
 func StrToTime(layout, value string, loc ...*time.Location) (time.Time, error) {
-	location := time.Local
+	location := time.UTC
 	if len(loc) > 0 {
 		location = loc[0]
 	}
