f129b3eb4b40b130ae7c0eb3b80f5bebdbf7df6f
axiaoxin <254606826@qq.com>
2021-04-22 01:50:04 +0800

    方差标准差

--- statistics.go
+++ statistics.go
@@ -1,6 +1,9 @@
 package goutils
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 // AvgFloat64 平均值
 func AvgFloat64(f []float64) (float64, error) {
@@ -13,4 +16,32 @@ func AvgFloat64(f []float64) (float64, error) {
 		sum += i
 	}
 	return sum / float64(len(f)), nil
+}
+
+// VarianceFloat64 求方差
+func VarianceFloat64(fs []float64) (float64, error) {
+	fsLen := len(fs)
+	if fsLen < 2 {
+		return 0, errors.New("slice len must greater than 2")
+	}
+	// 均值
+	favg, err := AvgFloat64(fs)
+	if err != nil {
+		return 0, err
+	}
+	variance := float64(0)
+	for _, f := range fs {
+		variance += math.Pow(f-favg, 2)
+	}
+	variance = variance / (float64(len(fs) - 1))
+	return variance, nil
+}
+
+// StdDeviationFloat64 求标准差
+func StdDeviationFloat64(fs []float64) (float64, error) {
+	v, err := VarianceFloat64(fs)
+	if err != nil {
+		return 0, err
+	}
+	return math.Sqrt(v), nil
 }
