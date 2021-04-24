9a5daf2566a5b5f11222973e747844c7b43de2e5
axiaoxin <254606826@qq.com>
2021-04-24 10:43:22 +0800

    struct tag list

--- struct.go
+++ struct.go
@@ -1,6 +1,46 @@
 package goutils
 
-import "reflect"
+import (
+	"net/url"
+	"reflect"
+	"strconv"
+)
+
+// StructToURLValues 将结构体指针对象转换为 url.Values ， key 为 json tag ， value 为结构体字段值，没有 json tag 则使用字段名称
+func StructToURLValues(item interface{}, tag string) (values url.Values) {
+	values = url.Values{}
+
+	iv := reflect.ValueOf(item).Elem() // Elem() 则 i 必须传指针，不使用 Elem() 则不传递指针
+	it := iv.Type()
+	for i := 0; i < iv.NumField(); i++ {
+		vf := iv.Field(i)
+		tf := it.Field(i)
+
+		k := tf.Tag.Get(tag)
+		if k == "" {
+			k = tf.Name
+		}
+
+		v := ""
+		switch vf.Interface().(type) {
+		case int, int8, int16, int32, int64:
+			v = strconv.FormatInt(vf.Int(), 10)
+		case uint, uint8, uint16, uint32, uint64:
+			v = strconv.FormatUint(vf.Uint(), 10)
+		case float32:
+			v = strconv.FormatFloat(vf.Float(), 'f', 4, 32)
+		case float64:
+			v = strconv.FormatFloat(vf.Float(), 'f', 4, 64)
+		case []byte:
+			v = string(vf.Bytes())
+		case string:
+			v = vf.String()
+		}
+
+		values.Set(k, v)
+	}
+	return
+}
 
 // StructToMapByTag 结构体转 Map
 func StructToMapByTag(item interface{}, tag string) map[string]interface{} {
@@ -28,4 +68,14 @@ func StructToMapByTag(item interface{}, tag string) map[string]interface{} {
 		}
 	}
 	return res
+}
+
+// StructTagList 获取结构体 tag 列表
+func StructTagList(item interface{}, tag string) []string {
+	tags := []string{}
+	s := reflect.TypeOf(item).Elem()
+	for i := 0; i < s.NumField(); i++ {
+		tags = append(tags, s.Field(i).Tag)
+	}
+	return tags
 }
