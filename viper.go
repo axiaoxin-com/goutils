2e0fbee8f6aeeefead9e92aa3d8ef9af7a0f6bf5
axiaoxin <254606826@qq.com>
2022-04-19 15:14:00 +0800

    init viper param update

--- viper.go
+++ viper.go
@@ -3,16 +3,26 @@
 package goutils
 
 import (
+	"path"
+	"strings"
+
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
 // InitViper 根据配置文件路径和名称初始化 viper 并监听变化
-// configPath 配置文件路径
-// configName 配置文件名（不带格式后缀）
-// configType 配置文件格式后缀
+// configFile 配置文件
 // onConfigChangeRun 配置文件发生变化时的回调函数
-func InitViper(configPath, configName, configType string, onConfigChangeRun func(fsnotify.Event)) error {
+func InitViper(configFile string, onConfigChangeRun func(fsnotify.Event)) error {
+	// 加载配置文件内容到 viper 中以便使用
+	configPath, file := path.Split(configFile)
+	if configPath == "" {
+		configPath = "./"
+	}
+	ext := path.Ext(file)
+	configType := strings.Trim(ext, ".")
+	configName := strings.TrimSuffix(file, ext)
+
 	viper.AddConfigPath(configPath)
 	viper.SetConfigName(configName)
 	viper.SetConfigType(configType)
